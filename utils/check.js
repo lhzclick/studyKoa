f247220098c0357fc53fc288fff4797a217db176
liuhu <392539910@qq.com>
2019-09-27 16:57:03 +0800

    token拦截器优化

--- utils/check.js
+++ utils/check.js
@@ -1,7 +1,14 @@
-const Promise = require("bluebird");
-const jwt = require("jsonwebtoken");
-const verify = Promise.promisify(jwt.verify);
-let secret = 'jwt demo'
+//  拦截器-- token
+
+// const Promise = require("bluebird");
+// const jwt = require("jsonwebtoken");
+// const verify = Promise.promisify(jwt.verify);
+// let secret = 'jwt demo'
+
+
+// jwt
+const jwt = require('jsonwebtoken')
+const secret = 'jwt demo'
 
 async function check(ctx, next) {
     let url = ctx.request.url;
@@ -9,27 +16,49 @@ async function check(ctx, next) {
     if(url == "/login") {
         await next();
     }else {
-        let token = ctx.header.token;
-        if(token){
-            let payload = await verify(token,secret);
-            let {time, timeout} = payload;
-            let dataTime = new Date().getTime();
-            if(dataTime - time <= timeout) {
-                //存在且未过期
+        // let token = ctx.header.token;
+        // if(token){
+        //     let payload = await verify(token,secret);
+        //     let {time, timeout} = payload;
+        //     let dataTime = new Date().getTime();
+        //     if(dataTime - time <= timeout) {
+        //         //存在且未过期
+        //         await next()
+        //     }else{
+        //         //过期
+        //         ctx.body = {
+        //             err: -1,
+        //             msg:'token 已过期'
+        //         };
+        //     }
+        // }else{
+        //     ctx.body = {
+        //         err: -1,
+        //         msg:'token不存在，请重新登录'
+        //     };
+        // }        
+
+
+        const token = ctx.header.token
+        if (token) {
+            // token校验通过
+            try {
+                let decode = jwt.verify(token, secret)
                 await next()
-            }else{
-                //过期
+            } catch (err) {
+                // token校验未通过
                 ctx.body = {
-                    err: -1,
-                    msg:'token 已过期'
-                };
+                    msg: 'token 错误',
+                    err: -101
+                }
             }
-        }else{
+        } else {
+            // token 不存在
             ctx.body = {
-                err: -1,
-                msg:'token不存在，请重新登录'
-            };
-        }        
+                msg: 'token不存在',
+                err: -101
+            }
+        }
     }
 }
 
